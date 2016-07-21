09fbdc885931503235697a942b9165fe3334379e
James Rowell <james.rowell@reelfx.com>
2016-07-21 09:49:31 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2.7
+
+from distutils.core import setup
+import os
+import shutil
+
+setup(name='renum',
+      version='0.100',
+      description='tool to renumber image sequences',
+      long_description='',
+      author='James Philip Rowell',
+      author_email='james@oic-inc.net',
+      url='http://www.orangeimagination.com/',
+      py_modules=[],
+      scripts=['renum'],
+      license = "BSD 3-Clause license",
+     )
