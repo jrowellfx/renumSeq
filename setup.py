80cb40d9c8353f24f27106fdaf68a0612ad79a54
James Philip Rowell <james.rowell@yahoo.com>
2018-10-23 22:21:33 -0500

    New copyright and tarball reflecting the change.

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ import os
 import shutil
 
 setup(name='renum',
-      version='1.011',
+      version='1.012',
       description='tool to renumber image sequences',
       long_description='Uses lsseq native format to specify sequence to renumber - with various options.  Very safe in terms on not overwriting files accidentally. lsseq must be installed on the system as renum uses a python library (seqLister) that is installed by that package',
       author='James Philip Rowell',
-      author_email='james@oic-inc.net',
-      url='http://www.orangeimagination.com/',
+      author_email='james@alpha-eleven.com',
+      url='http://www.alpha-eleven.com/',
       py_modules=[],
       scripts=['renum'],
       license = "BSD 3-Clause license",
